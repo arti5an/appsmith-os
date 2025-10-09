18e213b94280e1eb96ca4688374922df71ffb755
αяτιsαη <artis4n@outlook.com>
2025-10-10 00:09:47 +0100

    Add qutebrowser dnf package

--- build.sh
+++ build.sh
@@ -10,7 +10,7 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf install -y fscrypt
+dnf install -y fscrypt qutebrowser
 
 # Use a COPR Example:
 #
