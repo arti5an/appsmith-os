b1ead84bd585c942fd65e0bbd6e3ee66fba9dc10
Richard Smith <artis4n@outlook.com>
2025-11-06 03:28:54 +0000

    Start implementing DankMaterialShell

--- build_files/build.sh
+++ build_files/build.sh
@@ -9,15 +9,29 @@ set -ouex pipefail
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/42/x86_64/repoview/index.html&protocol=https&redirect=1
 
-dnf5 install -y libffi-devel libyaml-devel \
+dnf5 install -y libffi-devel libyaml-devel mate-polkit \
   chafa flac fscrypt fuse-sshfs fuse-zip sshfs vifm \
   gwenview mako niri okular qutebrowser
 
-# Zellij
+# Ghostty - https://ghostty.org/
+dnf5 -y copr enable scottames/ghostty
+dnf5 -y install ghostty
+dnf5 -y copr disable scottames/ghostty
+
+# Zellij - https://zellij.dev/
 dnf5 -y copr enable varlad/zellij
 dnf5 -y install zellij
 dnf5 -y copr disable varlad/zellij
 
+# DankMaterialShell - https://danklinux.com/
+dnf5 -y copr enable avengemedia/dms
+dnf5 -y install dms dms-greeter
+dnf5 -y copr disable avengemedia/dms
+dnf5 -y copr enable avengemedia/danklinux
+dnf5 -y install dgop dsearch
+dnf5 -y copr disable avengemedia/danklinux
+
 #### Example for enabling a System Unit File
 
-# systemctl enable podman.socket
+systemctl disable gdm.service
+systemctl enable greetd.service
