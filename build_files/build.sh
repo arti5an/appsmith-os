f3c777797023c9cc5509a701d4e264d8b1f1c615
Richard Smith <appsmithuk@outlook.com>
2026-01-22 23:56:57 +0000

    Trying Aurora again - it's been over a year!
    Found Gnome kinda infuriating - extension soup leading to a messy
    desktop! Yearned for more controllable times, so tried Omarchy, then
    Arch raw, then NixOS, then Arch again, then NixOS again, then even
    Fedora Atomic Sway(!) All are great for learning the intricacies of
    Linux, but when you just want the damn file explorer to show remote
    thumbnails, or even open a bloody remote file... Why does it have to be
    so difficult?
    
    Bluefin wasn't perfect, but I probably revamped my Linux knowledge on
    this journey to work out the kinks, but it's still Gnome, so trying KDE
    via Aurora instead... Fingers crossed!

--- build_files/build.sh
+++ build_files/build.sh
@@ -9,9 +9,8 @@ set -ouex pipefail
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
 
-dnf5 install -y libffi-devel libicu74 libjpeg-turbo-devel libyaml-devel mate-polkit \
-  chafa flac fscrypt fuse-sshfs fuse-zip sshfs vifm \
-  gwenview mako okular qutebrowser
+dnf5 install -y libffi-devel libicu74 libjpeg-turbo-devel libyaml-devel \
+  chafa flac fscrypt fuse-sshfs fuse-zip sshfs vifm qutebrowser
 
 # Ghostty - https://ghostty.org/
 dnf5 -y copr enable scottames/ghostty
